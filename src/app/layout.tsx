c9becdf869e7c0c85fc8f9ddd917c8405095941c
kopz9 <gustavoeduardo438@gmail.com>
2024-04-19 20:58:16 -0300

    feat: database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans${inter.variable} flex flex-col gap-4`}>
+      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
         <TopNav />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/7eac6c32-38d7-471a-8974-5dfb172595d1-6oyrny.jpg",
@@ -12,10 +13,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts)
+
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name} </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
