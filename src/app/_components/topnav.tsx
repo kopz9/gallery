0fac2d699339b1b4a4eb36c6511f0a7a6a60724f
kopz9 <gustavoeduardo438@gmail.com>
2024-04-20 18:34:31 -0300

    feat: use auth for homepage

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -2,7 +2,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function TopNav() {
   return (
-    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+    <nav className="flex w-full items-center justify-between p-4 text-xl border-b font-semibold">
       <div>Gallery</div>
 
       <div>
