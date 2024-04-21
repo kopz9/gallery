fe2b1fa0a17ec8fd755a9f3ab5fcdde81148a04c
kopz9 <gustavoeduardo438@gmail.com>
2024-04-21 18:22:20 -0300

    feat: implement parallel routes

---
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { getImage } from "~/server/db/queries";
+
+export default async function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+  const idAsNumber = Number(photoId);
+
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
+
+  const image = await getImage(idAsNumber);
+
+  return (
+    <div>
+      <img src={image.url} className="w-96" />
+    </div>
+  );
+}
