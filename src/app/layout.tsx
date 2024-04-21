a552748c15ba19c7026bdc67fe84354d28829078
kopz9 <gustavoeduardo438@gmail.com>
2024-04-21 20:19:21 -0300

    feat: grid layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-          <TopNav />
-          {children}
+        <body className={`font-sans ${inter.variable}`}>
+          <div className='h-screen grid grid-rows-[auto,1fr]'>
+            <TopNav />
+            <main className='overflow-y-scroll'>{children}</main>
+          </div>
           {modal}
           <div id="modal-root" />
         </body>
